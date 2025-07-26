53a0886f2ca07250ab8372147098f64e807a4ca5
suhun <yunsuhun518@naver.com>
2025-07-26 21:00:48 +0900

    회원가입 페이지 수정 (성별, 생일 삭 제=> 전화번호, 이메일 추가)

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,7 @@ const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello, World!');
-});
+}); 
 
 app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
